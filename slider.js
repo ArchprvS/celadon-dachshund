1f496cf22471969a7a29c4d595a438923711a78f
ArchprvS <prvs.code@gmail.com>
2025-04-15 22:12:52 +0200

    15.04.25

--- slider.js
+++ slider.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const img = document.querySelector('.photo');
+    const text_h1 = document.querySelector('.img-text h1');
+    const text_p = document.querySelector('.img-text p');
 
     // .jpeg paths //
 
@@ -21,9 +23,15 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => {
             img.src = current_jpeg;
             img.classList.remove('switch_photo')
-        },300)
+        }, 300)
+        text_h1.classList.add('switch_text')
+        text_p.classList.add('switch_text')
+        setTimeout(() => {
+            text_h1.classList.remove('switch_text')
+            text_p.classList.remove('switch_text')
+        }, 800)
     }
 
-    setInterval(swap_jpeg, 3000)
+    setInterval(swap_jpeg, 4000)
    
 })
